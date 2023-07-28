8c9fdf4a199ada0b74c9eafc746299121ac1dc61
bernardo brotelli <b.brotelle@gmail.com>
2023-07-27 21:52:11 -0300

    Schema and test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_25_225350) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_27_223856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_225350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_castles_on_user_id"
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.float "total_rental"
+    t.bigint "user_id", null: false
+    t.bigint "castle_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["castle_id"], name: "index_rentals_on_castle_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_25_225350) do
   end
 
   add_foreign_key "castles", "users"
+  add_foreign_key "rentals", "castles"
+  add_foreign_key "rentals", "users"
 end
