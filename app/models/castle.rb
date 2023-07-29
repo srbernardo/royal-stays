6f98de55ca9b127cd75868000ca5daae3202f747
bernardo brotelli <b.brotelle@gmail.com>
2023-07-29 12:25:06 -0300

    Add the validates of photo presence

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -5,4 +5,5 @@ class Castle < ApplicationRecord
 
   validates :name, :address, presence: true
   validates :daily_rate, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 999999 }
+  validates :photo, presence: true
 end
