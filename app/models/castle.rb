6fa2afeddab0452c4dc0cf10a12a884a02188e1f
Fernando Mussoline <fmussoline@gmail.com>
2023-07-29 11:33:32 -0300

    Correct Castle Models

--- app/models/castle.rb
+++ app/models/castle.rb
@@ -1,6 +1,6 @@
 class Castle < ApplicationRecord
   belongs_to :user
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
 
   validates :name, :address, presence: true
   validates :daily_rate, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 999999 }
