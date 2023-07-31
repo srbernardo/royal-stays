b58335a7678a5fe730cd7b751fff64a14274ad0d
sabrina amaral <sabrinamaraldev@gmail.com>
2023-07-30 23:37:24 -0300

    add search to home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def home
+  end
+
+  def search
+    place = "%#{params[:place]}%"
+    @castles = Castle.where('name ILIKE ?', place) | Castle.where('address ILIKE ?', place)
   end
 end
