ceb5e1a6405fda7a8ff9ac2d045dc08056e3ac4f
bernardo brotelli <b.brotelle@gmail.com>
2023-08-03 20:54:26 -0300

    Add username to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :rentals
   has_many :castles
+
+  validates :username, presence: true, uniqueness: true
 end
